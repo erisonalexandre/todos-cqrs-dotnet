a4680a5f26218b0cfbac48b636646380131da2ec
erisonalexandre <eafpimenta@paschoalotto.com.br>
2022-12-03 13:05:41 -0300

    feat(Tests): Creating MarkTodoAsDoneHandleTests and UpdateTodoHandlerTests

---
+++ Todo.Domain.Tests/HandlerTests/MarkTodoAsDoneHandleTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Commands;
+using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
+
+namespace Todo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class MarkTodoAsDoneHandleTests
+    {
+        private readonly MarkTodoAsDoneCommand _invalidCommand = new MarkTodoAsDoneCommand(Guid.NewGuid(), "");
+        private readonly MarkTodoAsDoneCommand _validCommand = new MarkTodoAsDoneCommand(Guid.NewGuid(), "marcos");
+        private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+
+        public MarkTodoAsDoneHandleTests()
+        {
+            _invalidCommand.Validate();
+            _validCommand.Validate();
+        }
+
+        [TestMethod]
+        public void Given_a_valid_command_must_mark_the_task_as_done()
+        {
+            var result = (GenericCommandResult)_handler.Handle(_validCommand);
+            Assert.AreEqual(result.Success, true);
+        }
+
+        [TestMethod]
+        public void Given_an_invalid_command_must_not_mark_the_task_as_done()
+        {
+            var result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(result.Success, false);
+        }
+    }
+}
---
+++ Todo.Domain.Tests/HandlerTests/UpdateTodoHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Commands;
+using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
+
+namespace Todo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class UpdateTodoHandlerTests
+    {
+        private readonly UpdateTodoCommand _invalidCommand = new UpdateTodoCommand(Guid.NewGuid(), "", "marcos");
+        private readonly UpdateTodoCommand _validCommand = new UpdateTodoCommand(Guid.NewGuid(), "Título da tarefa", "marcos");
+        private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+
+        public UpdateTodoHandlerTests()
+        {
+            _invalidCommand.Validate();
+            _validCommand.Validate();
+        }
+
+        [TestMethod]
+        public void Given_a_valid_command_must_update_the_task()
+        {
+            var result = (GenericCommandResult)_handler.Handle(_validCommand);
+            Assert.AreEqual(result.Success, true);
+        }
+
+        [TestMethod]
+        public void Given_an_invalid_command_must_not_update_the_task()
+        {
+            var result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(result.Success, false);
+        }
+    }
+}
