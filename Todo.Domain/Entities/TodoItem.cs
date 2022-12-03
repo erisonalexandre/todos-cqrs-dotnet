91d182d7e98c31338526ef44d03fea48e9a03051
erisonalexandre <eafpimenta@paschoalotto.com.br>
2022-12-03 12:29:04 -0300

    feat(Entites): Create Entity and TodoItem

---
+++ Todo.Domain/Entities/TodoItem.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Todo.Domain.Entities
+{
+    public class TodoItem : Entity
+    {
+        public TodoItem(string title, DateTime date, string user)
+        {
+            Title = title;
+            Done = false;
+            Date = date;
+            User = user;
+        }
+
+        public string Title { get; set; }
+
+        public bool Done { get; private set; }
+
+        public DateTime Date { get; private set; }
+
+        public string User { get; private set; }
+
+        public void MarkAsDone()
+        {
+            Done = true;
+        }
+
+        public void MarkAsUndone()
+        {
+            Done = false;
+        }
+
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+        }
+    }
+}
