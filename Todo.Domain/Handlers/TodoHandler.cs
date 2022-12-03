8a8734ebf82edb72e96b98e5fc19733d764eb12e
erisonalexandre <eafpimenta@paschoalotto.com.br>
2022-12-03 12:32:44 -0300

    feat(Handlers): Creating contract Handler and TodoHandler

---
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,54 @@
+﻿using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Entities;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler :
+        Notifiable,
+        IHandler<CreateTodoCommand>,
+        IHandler<UpdateTodoCommand>
+    {
+        private readonly ITodoRepository _repository;
+
+        public TodoHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            command.Validate();
+            if (command.Invalid)
+            {
+                return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada!", command.Notifications);
+            }
+
+            var todo = new TodoItem(command.Title, command.Date, command.User);
+
+            _repository.Create(todo);
+
+            return new GenericCommandResult(true, "Tarefa salva com sucesso!", todo);
+        }
+
+        public ICommandResult Handle(UpdateTodoCommand command)
+        {
+            command.Validate();
+            if (command.Invalid)
+            {
+                return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada!", command.Notifications);
+            }
+
+            var todo = _repository.GetById(command.Id, command.User);
+
+            todo.UpdateTitle(command.Title);
+
+            _repository.Update(todo);
+
+            return new GenericCommandResult(true, "Tarefa salva com sucesso!", todo);
+        }
+    }
+}
