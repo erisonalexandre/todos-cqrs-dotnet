ad34eb152330a882c17068a407f62f2979a2ba4d
erisonalexandre <eafpimenta@paschoalotto.com.br>
2022-12-03 13:04:29 -0300

    feat(Tests): Creating TodoItemTests and CreateTodoHandleTests

---
+++ Todo.Domain.Tests/EntitiesTests/TodoItemTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Tests.EntitiesTests
+{
+    [TestClass]
+    public class TodoItemTests
+    {
+        private readonly TodoItem _validTodo = new TodoItem("Título aqui", DateTime.Now, "User");
+
+        [TestMethod]
+        public void Given_a_new_task_the_task_must_be_incomplete()
+        {
+            Assert.AreEqual(_validTodo.Done, false);
+        }
+
+        [TestMethod]
+        public void Given_a_new_task_the_task_must_be_created_today()
+        {
+            Assert.AreEqual(_validTodo.Date.Date, DateTime.Now.Date);
+        }
+
+        [TestMethod]
+        public void Given_a_new_task_the_task_must_be_created_by_the_user()
+        {
+            Assert.AreEqual(_validTodo.User, "User");
+        }
+
+        [TestMethod]
+        public void Given_a_new_task_the_task_must_have_a_title()
+        {
+            Assert.AreEqual(_validTodo.Title, "Título aqui");
+        }
+    }
+}
---
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Commands;
+using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
+
+namespace Todo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateTodoHandlerTests
+    {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "marcos", DateTime.Now);
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Título da tarefa", "marcos", DateTime.Now);
+        private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+
+        public CreateTodoHandlerTests()
+        {
+            _invalidCommand.Validate();
+            _validCommand.Validate();
+        }
+
+        [TestMethod]
+        public void Given_a_valid_command_must_create_the_task()
+        {
+            var result = (GenericCommandResult)_handler.Handle(_validCommand);
+            Assert.AreEqual(result.Success, true);
+        }
+
+        [TestMethod]
+        public void Given_an_invalid_command_must_not_create_the_task()
+        {
+            var result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(result.Success, false);
+        }
+
+    }
+}
