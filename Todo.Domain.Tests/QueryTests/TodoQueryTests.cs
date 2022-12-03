b5ee4af22181a75adeadf6efa4e8c5a53cc3788d
erisonalexandre <eafpimenta@paschoalotto.com.br>
2022-12-03 19:40:08 -0300

    feat(Queries): Creating TodoQueries and TodoQueriesTests

---
+++ Todo.Domain.Tests/QueryTests/TodoQueryTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Entities;
+using Todo.Domain.Queries;
+
+namespace Todo.Domain.Tests.QueryTests
+{
+
+    [TestClass]
+    public class TodoQueryTests
+    {
+        private List<TodoItem> _items;
+
+        public TodoQueryTests()
+        {
+            _items = new List<TodoItem>();
+            _items.Add(new TodoItem("Título aqui", DateTime.Now, "User"));
+            _items.Add(new TodoItem("Título aqui 2", DateTime.Now, "User"));
+            _items.Add(new TodoItem("Título aqui 3", DateTime.Now, "User"));
+            _items.Add(new TodoItem("Título aqui 4", DateTime.Now, "User"));
+            _items.Add(new TodoItem("Título aqui 5", DateTime.Now, "User"));
+            _items.Add(new TodoItem("Título aqui 6", DateTime.Now, "User"));
+            _items.Add(new TodoItem("Título aqui 7", DateTime.Now, "Erison"));
+            _items.Add(new TodoItem("Título aqui 8", DateTime.Now, "Erison"));
+            _items.Add(new TodoItem("Título aqui 9", DateTime.Now, "Erison"));
+            _items.Add(new TodoItem("Título aqui 10", DateTime.Now, "Erison"));
+
+            var item = new TodoItem("Título aqui 11", DateTime.Now, "Erison");
+            item.MarkAsDone();
+            _items.Add(item);
+        }
+
+        [TestMethod]
+        public void Given_a_query_must_return_only_from_user_erison()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAll("Erison"));
+            Assert.AreEqual(result.Count(), 5);
+        }
+
+        [TestMethod]
+        public void Given_a_query_must_return_only_from_user_user()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAll("User"));
+            Assert.AreEqual(result.Count(), 6);
+        }
+
+        [TestMethod]
+        public void Given_a_query_must_return_only_done_tasks_from_user_erison()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAllDone("Erison"));
+            Assert.AreEqual(result.Count(), 1);
+        }
+
+        [TestMethod]
+        public void Given_a_query_must_return_only_undone_tasks_from_user_erison()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAllUndone("Erison"));
+            Assert.AreEqual(result.Count(), 4);
+        }
+
+        [TestMethod]
+        public void Given_a_query_must_return_only_done_tasks_from_user_user()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAllDone("User"));
+            Assert.AreEqual(result.Count(), 0);
+        }
+
+        [TestMethod]
+        public void Given_a_query_must_return_only_undone_tasks_from_user_user()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAllUndone("User"));
+            Assert.AreEqual(result.Count(), 6);
+        }
+    }
+}
---
+++ Todo.Domain/Queries/TodoQueries.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Queries
+{
+    public static class TodoQueries
+    {
+        public static Expression<Func<TodoItem, bool>> GetAll(string user)
+        {
+            return x => x.User == user;
+        }
+
+        public static Expression<Func<TodoItem, bool>> GetAllDone(string user)
+        {
+            return x => x.Done && x.User == user;
+        }
+
+        public static Expression<Func<TodoItem, bool>> GetAllUndone(string user)
+        {
+            return x => !x.Done && x.User == user;
+        }
+
+        public static Expression<Func<TodoItem, bool>> GetByPeriod(string user, DateTime date, bool done)
+        {
+            return x => x.Done == done && x.User == user && x.Date.Date == date.Date;
+        }
+    }
+}
